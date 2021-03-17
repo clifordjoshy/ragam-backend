5e4f4d264ae6975ac4c77c30bf76585f28df1c34
Cliford <clifordjo@gmail.com>
2021-03-17 10:45:10 +0530

    Changed server url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://f4a9f4564320.ngrok.io',
+  url: 'http://api.staging.ragam.live',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bb48de1c797c07948ea23f1ed31fa7b3'),
